0b2d4c0183a401fee45c573372dfc3887c9d78bf
Andreas Balevik <andreasbalevik@gmail.no>
2024-11-01 12:47:11 +0100

    added heading color

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       colors: {
         'primary': 'rgb(var(--color-primary,26,86,219),1)',
         'primary--hover': 'rgb(var(--color-primary,26,86,219),.9)',
+        'primary--heading': 'rgb(var(--color-primary,26,86,219),.9)',
         'primary--light': 'rgb(var(--color-primary,26,86,219),.05)'
       },
     },
