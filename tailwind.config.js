48b38bce5853b4cf0b34c631dcc309ec58991605
Andreas Balevik <andreasbalevik@gmail.no>
2024-11-01 11:19:56 +0100

    bumped theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,8 @@ module.exports = {
        }
       },
       colors: {
-        'primary': 'rgb(var(--color-primary,26,86,219),.9)',
-        'primary--hover': 'rgb(var(--color-primary,26,86,219),1)',
+        'primary': 'rgb(var(--color-primary,26,86,219),1)',
+        'primary--hover': 'rgb(var(--color-primary,26,86,219),.9)',
         'primary--light': 'rgb(var(--color-primary,26,86,219),.05)'
       },
     },
